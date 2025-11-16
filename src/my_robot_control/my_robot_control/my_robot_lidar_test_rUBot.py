e55225acaf73f904139328f5269bfe52d020baf8
margapayerass <mpayercr68@alumnes.ub.edu>
2025-11-16 16:41:37 +0000

    mod

--- src/my_robot_control/my_robot_control/my_robot_lidar_test_rUBot.py
+++ src/my_robot_control/my_robot_control/my_robot_lidar_test_rUBot.py
@@ -28,16 +28,16 @@ class LidarTest(Node):
 
         # Indices for specific angles in rUBot (-180deg to 180deg at 0.5deg/index)
         index_0_deg = int((0 - angle_min_deg + 180) / angle_increment_deg)
-        index_neg90_deg = int((-90 - angle_min_deg) / angle_increment_deg)
-        index_pos90_deg = int((90 - angle_min_deg) / angle_increment_deg)
+        index_neg90_deg = int((-90 - angle_min_deg + 180) / angle_increment_deg)
+        index_pos90_deg = int((90 - angle_min_deg - 180) / angle_increment_deg)
         dist_0_deg = scan.ranges[index_0_deg]
         dist_neg90_deg = scan.ranges[index_neg90_deg]
         dist_pos90_deg = scan.ranges[index_pos90_deg]
 
         self.get_logger().info("---- LIDAR readings ----")
         self.get_logger().info(f"Distance at 0°: {dist_0_deg:.2f} m" if dist_0_deg else "No valid reading at 0°")
-        self.get_logger().info(f"Distance at 90°: {dist_neg90_deg:.2f} m" if dist_neg90_deg else "No valid reading at -90°")
-        self.get_logger().info(f"Distance at -90°: {dist_pos90_deg:.2f} m" if dist_pos90_deg else "No valid reading at +90°")
+        self.get_logger().info(f"Distance at -90°: {dist_neg90_deg:.2f} m" if dist_neg90_deg else "No valid reading at -90°")
+        self.get_logger().info(f"Distance at 90°: {dist_pos90_deg:.2f} m" if dist_pos90_deg else "No valid reading at +90°")
 
         custom_range = []
 
